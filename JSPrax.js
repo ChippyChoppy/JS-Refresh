31aed3b4354f256885cea07b356356fa189b804e
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-07-04 13:50:59 -0400

    basics

---
+++ JSPrax.js
@@ -0,0 +1,32 @@
+let coolbeans = 'pintos'; // in-line comment
+console.log(coolbeans)
+/* Whoa
+a multi-line
+comment
+fancy pants */
+
+/* Data Types: anything meaningful to the computer
+undefined (something that hasn't been defined), null (nothing), boolean (true/false), string (text), symbol (immutable primitive value that is unique), number (a number), object (stores key:value pairs)
+
+variable - like a box you fill with any data you want
+*/
+var name = "first"; // used throughout whole program
+
+let myName = "Bex"; // only used in the scope where declared
+myName = "Rebex";
+
+const yourName = "cool"; // variable can't and won't ever change, trying to change const = error
+
+let a; // declaring a variable
+console.log(a)
+
+let b = 2 // assigning a variable
+console.log(b)
+a = 7 // assigning 
+
+b = a // assigning
+console.log(a, b)
+console.log
+coolbeans = 'turtle'
+
+console.log(coolbeans)
