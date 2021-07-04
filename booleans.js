272a26a690f28809c82dcecaaa113309633c41b5
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-07-04 15:23:06 -0400

    elses and ifs and else ifs oh my

--- booleans.js
+++ booleans.js
@@ -14,7 +14,7 @@ function trueOrFalse(isItTrue) {
 }
 console.log(trueOrFalse(13))
 function trueFalse(wasThatTrue) {
-    if(wasThatTrue) {
+    if (wasThatTrue) {
         return "yes, that was true";
     }
     return "no, that was false";
@@ -24,14 +24,14 @@ console.log(trueFalse(true))
 //comparison w/ equality operator
 
 function testEqual(val) {
-    if(val == 12) {
+    if (val == 12) {
         return "Equal";
     }
     return "Not Equal";
 }
 console.log(testEqual("12"))
 
-function strictEqual(val){
+function strictEqual(val) {
     if (val === 7) {
         return "equal"
     }
@@ -43,7 +43,7 @@ console.log(strictEqual("7"))
 //inequality operator
 
 function notEqual(val) {
-    if(val != 99) {
+    if (val != 99) {
         return "not equal"
     } return "equal"
 }
@@ -61,11 +61,31 @@ console.log(strictInequal(16))
 
 //comparisons w/ logical operators <, >, <=, >=
 function testGreater(val) {
-    if(val > 100) return "over 100";
-    if(val > 10) return "over 10";
+    if (val > 100) return "over 100";
+    if (val > 10) return "over 10";
     else return "less than 10"
 }
 console.log(testGreater(1))
 console.log(testGreater(13))
 console.log(testGreater(101))
 
+// && operator
+
+if (val <= 50 && val >= 25) {
+    return "yes"
+}
+
+// || or operator
+if (val < 10 || val > 20) return "outside"
+
+// if else
+
+if (val > 5) return "bigger than 5"
+else return "5 or smaller"
+
+// else if statements
+function elseIfs() {
+    if (val > 10) return "greater than 10";
+    else if (val < 5) return "smaller than 5"
+    else return "between 5 & 10"
+}
